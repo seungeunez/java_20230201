821402c19bd80a70f7d4f5d1579324118ab8888c
탁승은 <xkrtmddms@gmail.com>
2023-02-01 11:02:26 +0900

    클래스

--- src/cls/BookStore.java
+++ src/cls/BookStore.java
@@ -18,36 +18,41 @@ public class BookStore {
 	private String phone = " ";
 	private String address = " ";
 	private Book[] books = null; // 내가 만들어 놓은 클래스를 이렇게도 사용 가능
-	
-	
-	public int idx = 0; //현재 배열에 추가되어 있는 책의 위치를 변수로 생성
-	
-	//생성자는 최초 한 번만 사용 가능하다, 반환값(void)이 없다, 클래스명과 정확하게 일치해야 한다.
-	//준비물이 없는 빈 생성자
+
+	public int idx = 0; // 현재 배열에 추가되어 있는 책의 위치를 변수로 생성
+
+	// 생성자는 최초 한 번만 사용 가능하다, 반환값(void)이 없다, 클래스명과 정확하게 일치해야 한다.
+	// 준비물이 없는 빈 생성자
 	public BookStore() {
-		//super();	//지워도되는거임
-		
+		// super(); //지워도되는거임
+
 		this.books = new Book[100];
 	}
-	
-	
-	//책 등록하는 메소드(함수) 기능 필요
+
+	// 책 등록하는 메소드(함수) 기능 필요
 	public void insertBook(Book book) {
-		if(idx >= 100) {
+		if (idx >= 100) {
 			System.out.println("책 추가 불가");
-		}else {
+		} else {
 			this.books[idx] = book;
 			idx++;
 		}
-		
 	}
-	
-	//현재까지 등록된 책 목록을 출력
-	public void selectBook() {	//올게 없기에 ()는 비어있는게 맞음 올게 있으면 ()채워넣어야함
+
+	// 현재까지 등록된 책 목록을 출력
+	public void selectBook() { // 올게 없기에 ()는 비어있는게 맞음 올게 있으면 ()채워넣어야함
 		int i = 0;
-		for(i=0;i<idx;i++) {
+		for (i = 0; i < idx; i++) {
 			System.out.println(this.books[i].toString());
 		}
+	}
+	
+	// 마지막 등록한 책을 삭제하는 기능
+	public void deleteBook() {
+		if(idx > 0 ) {
+			idx--;
+		}
+		
 	}
 	
 	
--- src/main2/Main2.java
+++ src/main2/Main2.java
@@ -1,34 +1,52 @@
 package main2;
 
+import java.util.Date;
+
+import cls.Book;
 import cls.BookStore;
 
 public class Main2 {
 
 	public static void main(String[] args) {
-		
-		
+
 //		Scanner sc = new Scanner(System.in); //장치로부터 데이터를 받는것이기 때문에 ()안에 뭔갈 채워 넣었음
-		
-		//클래스명 객체명 = new 생성자명(준비물); 
+
+		// 클래스명 객체명 = new 생성자명(준비물);
 //		Book book = new Book(1, "java","가나다",12000L,'B',new Date());	// 괄호에 넣기 위해서 Book클래스에서 세팅해줘야함
-		
-		//위 괄호에 넣음으로 써 충족된거임
+
+		// 위 괄호에 넣음으로 써 충족된거임
 //		book.setNo(1);
 //		book.setTitle("JAVA");	
 //		book.setAuthor("가나다");
 //		book.setPrice(12000L);
 //		book.setCate('B');
 //		book.setDate(new Date());
-		
-		//서점이 생성됨 책을 100권 보관할 수 있는 배열생성
+
+		// 서점이 생성됨 책을 100권 보관할 수 있는 배열생성
 		BookStore bookStore = new BookStore();
-		
+
 		bookStore.setPhone("051-7070-7070");
 		bookStore.setAddress("부산 남구");
+
+		// 추가하고자 하는 책을 생성
+		Book book = new Book(100, "java", "저자", 1230, 'B', new Date());
+
+		// 서점에 1권의 책 등록
+		bookStore.insertBook(book);
 		
+		// 문제1 임의의 책을 2권 생성한후 서점에 등록하기
+		Book book1 = new Book(101, "Python", "가나", 10000, 'A', new Date());
+		Book book2 = new Book(150, "Spring", "다라", 12300, 'C', new Date());
 		
-		//추가하고자 하는 책을 생성
-		bookStore.insertBook(null);
+		bookStore.insertBook(book1);
+		bookStore.insertBook(book2);
+		
+		//책 삭제
+		bookStore.deleteBook();
+
+		// 현재까지 등록된 책 조회
+		bookStore.selectBook();
+		
 		
 	}
 
